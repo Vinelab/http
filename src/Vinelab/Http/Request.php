c252c74ceb319452b6cc4c260010c190fe35bd0f
Abed Halawi <halawi.abed@gmail.com>
2013-05-31 19:22:14 +0300

    Implement Request and Response Contracts, along with functionality to send request and transform/parse responses including headers with special JSON and XML content helpers

---
+++ src/Vinelab/Http/Request.php
@@ -0,0 +1,112 @@
+<?php namespace Vinelab\Http;
+
+use Vinelab\Http\Contracts\RequestInterface;
+
+Class Request implements RequestInterface{
+
+	/**
+	 * The default request schema
+	 * @var Array
+	 */
+	protected $default = [
+
+		'url'        => null,
+		'params' => [],
+		'headers'    => [],
+		'options'    => [],
+		'returnTransfer' => true,
+		'json'		 => false
+	];
+
+	/**
+	 * HTTP Request Method
+	 * @var string
+	 */
+	public $method = self::METHOD_GET;
+
+	/**
+	 * @var Array
+	 */
+	public $params = [];
+
+	/**
+	 * @var String
+	 */
+	public $url = null;
+
+	/**
+	 * Request Headers
+	 * @var Associative Array
+	 */
+	public $headers = [];
+
+	/**
+	 * Sets the request
+	 * @var boolean
+	 */
+	public $json= false;
+
+	/**
+	 * Return cURL transfer or not
+	 * @var boolean
+	 */
+	public $returnTransfer = true;
+
+	/**
+	 * @param Array $requestData
+	 */
+	function __construct($requestData)
+	{
+		$data = array_merge($this->default, $requestData);
+
+		$this->url     = $data['url'];
+		$this->method  = $data['method'];
+		$this->params  = $data['params'];
+		$this->headers = $data['headers'];
+		$this->json    = $data['json'];
+
+		if ($this->json)
+		{
+			array_push($this->headers, 'Content-Type: application/json');
+		}
+	}
+
+	public function send()
+	{
+		$cURLOptions = array(
+			CURLOPT_URL            => $this->url,
+			CURLOPT_RETURNTRANSFER => $this->returnTransfer,
+			CURLOPT_HTTPHEADER     => $this->headers,
+			CURLOPT_HEADER         => true,
+			CURLINFO_HEADER_OUT    => true
+		);
+
+		if ($this->method === static::method('POST'))
+		{
+			$cURLOptions[CURLOPT_POST] = count($this->params);
+			$cURLOptions[CURLOPT_POSTFIELDS] = http_build_query($this->params);
+
+		} elseif (count($this->params) > 0) {
+			$this->url = $this->url.'?'.http_build_query($this->params);
+			$cURLOptions[CURLOPT_URL] = $this->url;
+		}
+
+		// initialize cURL
+		$cURL = curl_init();
+		curl_setopt_array($cURL, $cURLOptions);
+
+		return Response::make($cURL);
+	}
+
+	/**
+	 * returns the value of an HTTP Verb constant of this class
+	 *
+	 * @param  string $method HTTP Verb
+	 * @return string
+	 */
+	public static function method($method)
+	{
+		$const = 'METHOD_'.strtoupper($method);
+		return constant('self::'.$const);
+	}
+}
---
+++ src/Vinelab/Http/Response.php
@@ -0,0 +1,143 @@
+<?php namespace Vinelab\Http;
+
+use Vinelab\Http\Contracts\ResponseInterface;
+
+Class Response implements ResponseInterface {
+
+	/**
+	 * The result coming from curl_getinfo()
+	 * @var Array
+	 */
+	protected $info = [];
+
+	/**
+	 * Response Content (Body)
+	 * @var Mixed
+	 */
+	protected $content;
+
+	/**
+	 * Response Headers
+	 * @var String
+	 */
+	protected $headers = [];
+
+	/**
+	 * @param cURL Handle $cURL
+	 */
+	function __construct($cURL)
+	{
+		$response = curl_exec($cURL);
+
+		if (!curl_errno($cURL))
+		{
+			$this->info    = curl_getinfo($cURL);
+			$this->headers = $this->parseHeaders($response, $this->info['header_size']);
+			$this->content = $this->parseBody($response, $this->info['header_size']);
+
+		} else {
+
+			throw new \Exception(curl_error($cURL));
+		}
+
+		curl_close($cURL);
+	}
+
+	/**
+	 * Initiates a request
+	 * @param  cURL Handle $cURL
+	 * @return Response
+	 */
+	public static function make($cURL)
+	{
+		return new static($cURL);
+	}
+
+	/**
+	 * @return @var info
+	 */
+	public function info()
+	{
+		return $this->info;
+	}
+
+	public function statusCode()
+	{
+		return $this->info['http_code'];
+	}
+
+	public function contentType()
+	{
+		return $this->info['content_type'];
+	}
+
+	public function content()
+	{
+		return $this->content;
+	}
+
+	protected function parseHeaders($response, $headerSize)
+	{
+		$headers = substr($response, 0, $headerSize);
+		$parsedHeaders = [];
+
+		foreach (explode("\r\n",$headers) as $header)
+		{
+			if (strpos($header, ':'))
+			{
+				$nestedHeader = explode(':', $header);
+				$parsedHeaders[$nestedHeader[0]] = trim($nestedHeader[1]);
+			}
+		}
+
+		return $parsedHeaders;
+	}
+
+	public function headers()
+	{
+		return $this->headers;
+	}
+
+	public function header($name)
+	{
+		return (array_key_exists($name, $this->headers)) ? $this->headers[$name] : null;
+	}
+
+	/**
+	 * Parses the body (content) out of the response
+	 * @param  cURL Response $response
+	 * @param  cURL Header Size $headerSize
+	 * @return string
+	 */
+	public function parseBody($response, $headerSize)
+	{
+		return substr($response, $headerSize);
+	}
+
+	/**
+	 * Content encoded as JSON
+	 * @return string
+	 */
+	public function json()
+	{
+		return json_decode($this->content);
+	}
+
+	/**
+	 * Content encoded as XML
+	 * @return SimpleXMLElement
+	 */
+	public function xml()
+	{
+		return new \SimpleXMLElement($this->content);
+	}
+
+	/**
+	 * Primary IP
+	 * @return string
+	 */
+	public function ip()
+	{
+		return $this->info['primary_ip'];
+	}
+}
