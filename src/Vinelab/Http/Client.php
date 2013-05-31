c993a74a174998d57ca3ef937db28f769e9e48c4
Abed Halawi <halawi.abed@gmail.com>
2013-05-31 19:31:28 +0300

    Implement the Client class to send requests using the __call() magic method. Also include tests for primary methods

--- src/Vinelab/Http/Client.php
+++ src/Vinelab/Http/Client.php
@@ -2,7 +2,49 @@
 
 Class Client {
 
+	/**
+	 * Performs a GET reuqest
+	 * @param  URL|Array $request
+	 * @return \Vinelab\Http\Response
+	 */
 
-	public function get(){}
-	public function post(){}
+	/**
+	 * Validates passed request parameters
+	 * @param  URL|Array $request
+	 * @return Boolean
+	 */
+	protected function valid($request)
+	{
+		return (boolean) array_key_exists('url', $request);
+	}
+
+	/**
+	 * Makes a Vinelab\Http\Request object out of an array
+	 * @param  [type] $request [description]
+	 * @return Vinelab\Http\Request          [description]
+	 */
+	private function requestInstance($request)
+	{
+		return new Request($request);
+	}
+
+	public function __call($method, $arguments)
+	{
+		$request = array_pop($arguments);
+
+		// add support to sending requests to url's only
+		if(!is_array($request))
+		{
+			$request = ['url'=>$request];
+		}
+
+		if ($this->valid($request))
+		{
+			$request['method'] = Request::method($method);
+			$this->request = $this->requestInstance($request);
+			return $this->request->send();
+		}
+
+		throw new \Exception('Invalid Request Params sent to HttpClient');
+	}
 }
---
+++ tests/ClientTest.php
@@ -0,0 +1,162 @@
+<?php
+
+use Vinelab\Http\Client;
+use Mockery as M;
+
+Class ClientTest extends PHPUnit_Framework_TestCase {
+
+	static $pidfile = './tests/server/.pidfile';
+	static $serverHost = 'localhost';
+	static $serverPort = '6767';
+
+	public static function setUpBeforeClass()
+	{
+		static::bootUpBuiltInServer();
+	}
+
+	public static function tearDownAfterClass()
+	{
+		static::turnDownBuiltInServer();
+	}
+
+	public function setUp()
+	{
+		$this->client = new Client;
+	}
+
+	public function testHeaders()
+	{
+		$request = ['url'=>'http://'.static::serverURL().'/get.php'];
+		$headers = $this->client->get($request)->headers();
+
+		$this->assertArrayHasKey('Host', $headers, 'Headers must have Host');
+		$this->assertEquals(static::$serverHost, $headers['Host']);
+
+		// custom headers
+		$request['url'] = 'http://'.static::serverUrl().'/content_type.php';
+		$headers = $this->client->post($request)->headers();
+
+		$this->assertArrayHasKey('Content-Type', $headers);
+		$this->assertEquals('application/json', $headers['Content-Type']);
+	}
+
+	public function testGetRequestWithoutParams()
+	{
+		$request = ['url'=>'http://'.static::serverURL().'/get.php'];
+		$this->assertNotNull($this->client->get($request)->content());
+	}
+
+	public function testGetRequestWithParams()
+	{
+		$request = [
+			'url' => 'http://'.static::serverURL().'/get.php',
+			'params' => [
+
+				'id'     => '12350ME1D',
+				'lang'   => 'en-us',
+				'format' => 'rss_200'
+			]
+		];
+
+		$content = $this->client->get($request)->content();
+		$this->assertNotNull($content);
+
+		$this->assertContains('id', $content, 'Should return the id param');
+		$this->assertContains('lang', $content, 'Should return the lang param');
+		$this->assertContains('format', $content, 'Should return the format param');
+	}
+
+	public function testGetJSONRequest()
+	{
+		$request = [
+			'url' => 'http://'.static::serverURL().'/json_get.php',
+			'params' => [
+
+				'id'     => '12350ME1D',
+				'lang'   => 'en-us',
+				'format' => 'json'
+			]
+		];
+
+		$content = $this->client->get($request)->json();
+
+		$this->assertInstanceOf('stdClass', $content, 'Content should be returned as an object');
+		$this->assertObjectHasAttribute('id', $content);
+		$this->assertObjectHasAttribute('lang', $content);
+		$this->assertObjectHasAttribute('format', $content);
+	}
+
+	public function testPostRequestWithoutParams()
+	{
+		$request = ['url'=>'http://'.static::serverURL().'/post.php'];
+		$this->assertNotNull($this->client->get($request)->content());
+	}
+
+	public function testPostRequestWithparams()
+	{
+		$request = [
+			'url' => 'http://'.static::serverURL().'/post.php',
+			'params' => [
+
+				'id'     => '12350ME1D',
+				'lang'   => 'en-us',
+				'format' => 'json'
+			]
+		];
+
+		$content = $this->client->post($request)->content();
+
+		$this->assertContains('id', $content, 'Should return the id param');
+		$this->assertContains('lang', $content, 'Should return the lang param');
+		$this->assertContains('format', $content, 'Should return the format param');
+	}
+
+	public function testPostJSONRequest()
+	{
+		$request = [
+			'url' => 'http://'.static::serverURL().'/post.php',
+			'params' => [
+
+				'id'     => '12350ME1D',
+				'lang'   => 'en-us',
+				'format' => 'json'
+			],
+
+			'json' => true
+		];
+
+		$content = $this->client->post($request)->json();
+
+		$this->assertObjectHasAttribute('id', $content, 'Should return the id param');
+		$this->assertObjectHasAttribute('lang', $content, 'Should return the lang param');
+		$this->assertObjectHasAttribute('format', $content, 'Should return the format param');
+		// this one is added by the server
+		$this->assertObjectHasAttribute('json_header', $content, 'This should be returned when sending with a JSON header');
+	}
+
+	public static function bootUpBuiltInServer()
+	{
+		shell_exec('php -S '.static::serverURL().' -t ./tests/server > /dev/null 2>&1 & echo $! >> '.static::$pidfile);
+		// let's wait a couple of seconds for the server to be up
+		sleep(2);
+	}
+
+	public static function turnDownBuiltInServer()
+	{
+		$filename = static::$pidfile;
+
+		if(file_exists($filename))
+		{
+			$fileContent = file($filename);
+			$pid = array_pop($fileContent);
+			shell_exec('kill -9 '.$pid);
+			unlink($filename);
+		}
+	}
+
+	public static function serverURL()
+	{
+		return static::$serverHost.':'.static::$serverPort;
+	}
+
+}
