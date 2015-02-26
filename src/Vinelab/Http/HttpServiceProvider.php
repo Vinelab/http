9231176e7bb1fcc683904ab781f2f033c47333ca
Abed Halawi <halawi.abed@gmail.com>
2015-02-26 19:08:05 +0200

    remove the package method from service provider

--- src/Vinelab/Http/HttpServiceProvider.php
+++ src/Vinelab/Http/HttpServiceProvider.php
@@ -12,16 +12,6 @@ class HttpServiceProvider extends ServiceProvider {
 	protected $defer = false;
 
 	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('vinelab/http');
-	}
-
-	/**
 	 * Register the service provider.
 	 *
 	 * @return void
